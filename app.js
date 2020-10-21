dadf398370794ee6dc93f0396c848e893a783d5d
MichelleRoubikLee <michelleroubiklee@gmail.com>
2020-10-21 17:03:13 -0500

    added choice variables

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict"
+
+//destinations
+let dest = ["Hogsmeade","Hogwarts","Kings Cross","Shreeking Shack","Private Drive","The Burrow", "Diagon Alley"];
+//resturants
+let rest = ["Leaky Cauldron", "Great Hall", "The Hogs Head", "Honeydukes","The Hogwarts Kitchens"];
+//transportation
+let transport = ["Hogwarts Express", "Ford Anglia","Apparition","Broom","Flying Motorcycle", "Floo Powder"];
+//entertainment
+let entertain = ["Quidditch Match","de-Gnoming","Tri-Wizzard Tournament"];
